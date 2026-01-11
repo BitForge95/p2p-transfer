8efc0396266c940d556ebb61a9aba44a0b0f1345
Charan Sai <guttulacharansai@gmail.com>
2026-01-12 00:21:21 +0530

    Revert test file to maintain original behavior

--- src/test/java/com/BencodeParserTest.java
+++ src/test/java/com/BencodeParserTest.java
@@ -4,15 +4,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import com.BencodeParser;
 
-public class BencodeParserTest {
-    @Test
-    void testString() {
-        BencodeParser parser = new BencodeParser("4:spam".getBytes());
-        byte[] result = (byte[]) parser.decode();
-        assertEquals("spam", new String(result));
-            
-        parser = new BencodeParser("0:".getBytes());
-        result = (byte[]) parser.decode();
-        assertEquals("", new String(result));
-    }
+@Test
+void testString() {
+    BencodeParser parser = new BencodeParser("4:spam".getBytes());
+    byte[] result = (byte[]) parser.decode();
+    assertEquals("spam", new String(result));
+        
+    parser = new BencodeParser("0:".getBytes());
+    result = (byte[]) parser.decode();
+    assertEquals("", new String(result));
 }
