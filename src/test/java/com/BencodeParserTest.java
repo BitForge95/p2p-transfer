cbab3f3b7204afbfdbb0da29991eba3c32d6cd29
Charan Sai <guttulacharansai@gmail.com>
2026-01-12 00:20:32 +0530

    Upgrade Java to version 21

--- src/test/java/com/BencodeParserTest.java
+++ src/test/java/com/BencodeParserTest.java
@@ -4,13 +4,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import com.BencodeParser;
 
-@Test
-void testString() {
-    BencodeParser parser = new BencodeParser("4:spam".getBytes());
-    byte[] result = (byte[]) parser.decode();
-    assertEquals("spam", new String(result));
-        
-    parser = new BencodeParser("0:".getBytes());
-    result = (byte[]) parser.decode();
-    assertEquals("", new String(result));
+public class BencodeParserTest {
+    @Test
+    void testString() {
+        BencodeParser parser = new BencodeParser("4:spam".getBytes());
+        byte[] result = (byte[]) parser.decode();
+        assertEquals("spam", new String(result));
+            
+        parser = new BencodeParser("0:".getBytes());
+        result = (byte[]) parser.decode();
+        assertEquals("", new String(result));
+    }
 }
