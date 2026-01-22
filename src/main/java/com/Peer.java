3806070412fd835f69abd8382f68fa8c622c79c1
Charan Sai <guttulacharansai@gmail.com>
2026-01-22 22:59:09 +0530

    feat : Create Peer Data Model

---
+++ src/main/java/com/Peer.java
@@ -0,0 +1,19 @@
+package com;
+
+public class Peer {
+    private final String ip;
+    private final int port;
+
+    public Peer(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
+    public String getIp() { return ip; }
+    public int getPort() { return port; }
+
+    @Override
+    public String toString() {
+        return ip + ":" + port;
+    }
+}
